dfbc5852f23117959200d51a7c13857fb15fac99
NikitaMigushev <migushev@gmail.com>
2020-03-22 21:48:33 +0300

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,50 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  // throw 'Not implemented';
+
+  if (Array.isArray(members)) {
+    let result = [];
+    members.map((element, index) => {
+      let type = typeof (element);
+      if (type === 'string') {
+        let split = element.split('');
+        let firstCharacter = split[0];
+        result.push(firstCharacter);
+      } else {
+
+      }
+    })
+    return result.sort();
+  } else {
+    return false;
+  }
+
 };
+
+const example = [
+  'Amelia',
+  null,
+  undefined,
+  'Ruby',
+  'Lily',
+  11,
+  'Grace',
+  22,
+  'Millie',
+  'Daisy',
+  true,
+  'Freya',
+  false,
+  'Erin',
+  new Set([1, 2, 3, 4, 5]),
+  'Megan',
+  {
+    'John': 'Smith'
+  },
+  'Jasmine',
+  1,
+  2,
+  3,
+  4,
+  5,
+  'Brooke',
+]
