842b984289880ef02e975bac554eb1437ec4a320
NikitaMigushev <migushev@gmail.com>
2020-03-22 12:46:27 +0300

    feat: solve cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,27 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  //throw 'Not implemented';
+  let result = 0;
+  if (!matrix || matrix.length <= 0) {
+    return 0;
+  } else {
+    matrix.map((element) => {
+      element.forEach(el => {
+        if (el === "^^") {
+          result += 1
+        } else {};
+      });
+    })
+  }
+  return result;
 };
+
+
+const example = [
+  ['##', 'dd', '00'],
+  ['^^', '..', 'ss'],
+  ['AA', 'dd', 'Oo'],
+];
+
+const example2 = [
+  []
+];
