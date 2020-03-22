45ed91b297ab6daaf028543ca65091d284b630f7
NikitaMigushev <migushev@gmail.com>
2020-03-22 22:15:14 +0300

    fix: fix dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -7,44 +7,32 @@ module.exports = function createDreamTeam(members) {
       let type = typeof (element);
       if (type === 'string') {
         let split = element.split('');
-        let firstCharacter = split[0];
+        let firstCharacter = split[0].toUpperCase();
         result.push(firstCharacter);
       } else {
 
       }
     })
-    return result.sort();
+
+    return result.sort().join('');
   } else {
     return false;
   }
 
 };
 
-const example = [
-  'Amelia',
-  null,
-  undefined,
-  'Ruby',
-  'Lily',
-  11,
-  'Grace',
-  22,
-  'Millie',
-  'Daisy',
-  true,
-  'Freya',
-  false,
-  'Erin',
-  new Set([1, 2, 3, 4, 5]),
-  'Megan',
-  {
-    'John': 'Smith'
-  },
-  'Jasmine',
-  1,
-  2,
-  3,
-  4,
-  5,
-  'Brooke',
-]
+// const example = [
+//   'amelia',
+//   'ruby',
+//   'lily',
+//   'grace',
+//   'millie',
+//   'daisy',
+//   'freya',
+//   'erin',
+//   'megan',
+//   'jasmine',
+//   'brooke',
+// ]
+
+// console.log(module.exports(example));
